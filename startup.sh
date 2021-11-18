5c1cc2471ca5429e7201b52521c75b884c7057e2
adp8ke <adp8ke@gmail.com>
2021-11-18 20:28:44 +0000

    adding config file for returning users

--- startup.sh
+++ startup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-# Ask the user for their name
-read -p "Client ID: " clientId
-read -sp "Client Secret: " clientSecret
-echo
-echo "Drag-and-drop your secure connect bundle into the root directory and copy the path"
-read -p "Path for Secure Connect Bundle: " secureConnectBundlePath
-echo "Starting CQLSH"
-./cqlsh-astra/bin/cqlsh -u $clientId -p $clientSecret -b $secureConnectBundlePath
+
+echo 'Are your credentials already in the creds.config file and is the secure connect bundle uploaded? [yes/no]'
+read answer
+if [ $answer == 'yes' ]
+then 
+    . /workspace/example-datastax-astra-standalone-cqlsh/creds.config
+    echo "Starting CQLSH"
+    ./cqlsh-astra/bin/cqlsh -u $clientId -p $clientSecret -b $secureConnectBundlePath
+else
+    read -p "Client ID: " clientId
+    read -sp "Client Secret: " clientSecret
+    echo
+    echo "Drag-and-drop your secure connect bundle into the root directory and copy the path"
+    read -p "Path for Secure Connect Bundle: " secureConnectBundlePath
+    sed -i "s#{clientId}#$clientId#g; s#{clientSecret}#$clientSecret#g; s#{secureConnectBundlePath}#$secureConnectBundlePath#g" /workspace/example-datastax-astra-standalone-cqlsh/creds.config
+    echo 'Your crendentials have been saved to the creds.config file'
+    echo "Starting CQLSH"
+    ./cqlsh-astra/bin/cqlsh -u $clientId -p $clientSecret -b $secureConnectBundlePath
+fi
+
